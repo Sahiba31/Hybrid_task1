81b1f4db01e3625d56bdbd2b1b6989dfb0d00231
Sahiba31 <64470245+Sahiba31@users.noreply.github.com>
2020-06-15 00:15:10 +0530

    final

---
+++ index.php
@@ -0,0 +1,11 @@
+Welcome 
+<pre>
+<html>
+<body>
+<?php
+  print `ifconfig`;
+echo "<img src = 'https://sahiba-bucket1.s3.ap-south-1.amazonaws.com/Flower.jpg' , width='450' , height='500' />";
+?>
+</body>
+</html>
+</pre>
